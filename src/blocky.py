c9aa23ab168f9f45c5e30dbae4a72bafedacc812
Lubomir Milko <milko.lubomir@gmail.com>
2024-01-07 17:52:09 +0100

    Add license docstring to the main blocky source file

--- src/blocky.py
+++ src/blocky.py
@@ -1,13 +1,30 @@
 """
-Lightweight Python template engine.
+Blocky - Lightweight Python template engine.
+
+Copyright (C) 2024 Lubomir Milko
+This file is part of blocky <https://github.com/lubomilko/blocky>.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 from pathlib import Path
 from typing import Union, Callable
 
 __author__ = "Lubomir Milko"
-__copyright__ = "Copyright 2024, Lubomir Milko"
+__copyright__ = "Copyright (C) 2024 Lubomir Milko"
 __version__ = "1.0.0"
+__license__ = "GPLv3"
 
 
 class Tag:
